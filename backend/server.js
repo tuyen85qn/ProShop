9f38c3edde71e243e3d5dde3fe8f6038c8bd91e9
validName <tuyencntt85@gmail.com>
2020-11-16 21:54:41 +0700

    create seeder

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get('/',(req, res)=>{
     res.send("Api running .....");
@@ -15,4 +17,4 @@ app.get('/api/products/:id',(req,res)=>{
    res.json(product);   
 });
 const PORT = process.env.PORT || 5000
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}.`));
+app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}.`.yellow.underline));
